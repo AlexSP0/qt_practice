cdf2ffc1c79ee88339cac3fbc636ebde9a253314
Alexey Saprunov <workernight@yandex.ru>
2022-06-02 14:22:42 +0300

    lesson_03

---
+++ lesson_01/mainwindow.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+#include <QRegExpValidator>
+
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+        QString macByteRange= "[0-9|a-f|A-F][0-9|a-f|A-F]";
+        QString sep = "[-|:]";
+
+        QRegExp ipRegex (macByteRange + sep + macByteRange + sep + macByteRange + sep
+                         + macByteRange + sep + macByteRange + sep + macByteRange);
+
+        QRegExpValidator *ipValidator = new QRegExpValidator(ipRegex, this);
+
+        ui->lineEdit->setValidator(ipValidator);
+
+        connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::clickButton);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+{
+    ui->lineEdit->setText(ui->lineEdit->text().replace('-',':'));
+}
+
+void MainWindow::clickButton() {
+       qWarning("clicked");
+}
+
